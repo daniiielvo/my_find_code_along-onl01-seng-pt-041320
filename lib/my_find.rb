0fa410218682b165b75c196fab665db8668123c1
Daniel Vo <daniiielvo@gmail.com>
2020-04-23 18:14:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_find(collection)
 i = 0 
 collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
 while i < collection.length 
 yield(colletion[i])
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
 i = i + 1 
 end 
 end
